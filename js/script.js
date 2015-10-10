da924812ff35a3c5ebe49322f3b6b8ede7973825
Ростислав <extra0@mail.ru>
2015-10-10 19:50:24 +0300

    корзина шаг 1

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ $(function(){
 	// вызов фенсибокса
 	$('.fancybox').fancybox();
 
+	// показываем подробное описание услуг по посадке в корзине
+	$('.cart-step__plant-detail-link').click(function(){
+		$(this).next().slideToggle(500);
+	});
+
 });
