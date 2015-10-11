ead2f4f872faf415007c9d98966b729afc2a7b34
Ростислав <extra0@mail.ru>
2015-10-11 21:38:05 +0300

    страница продукта

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-$(function(){
+$(function() {
 
 	// вызов фенсибокса
 	$('.fancybox').fancybox();
 
 	// показываем подробное описание услуг по посадке в корзине
-	$('.cart-step__plant-detail-link').click(function(){
+	$('.cart-step__plant-detail-link').click(function() {
 		$(this).next().slideToggle(500);
 	});
 
 	// слайдер в продукте
-	
+	$('.slider-for').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+		slidesToShow: 3,
+		infinite: true,
+		variableWidth: true,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		dots: false,
+		centerMode: false,
+		focusOnSelect: true
+	});
 });
